c165f95bf515f906669c8be0985a19d9fbc9430e
tinevancorenland <tinevancorenland@gmail.com>
2019-07-25 09:41:02 +0200

    Update tine.js
    Add info to first table, working on simpler way and adapting y axis

--- tine.js
+++ tine.js
@@ -5,60 +5,175 @@ canvas.id = "myChart";
 var header = document.getElementsByTagName("h3")[0];
 header.appendChild(canvas);
 
-// ACCESS ELEMENTS TO INPUT THEM IN TABLE
+// YEARS FROM TABLE TO CHART LABEL
 
 var tableOne = document.getElementById("table1");
 console.log(tableOne);
 var tableOneBody = tableOne.getElementsByTagName("tbody");
-console.log(tableOneBody);
 var years = tableOneBody[0].childNodes[1];
-console.log(years);
 var correctYear = years.getElementsByTagName("th");
-console.log(correctYear);
+
+// var time = tableOne.rows.item(1).cells;
+// console.log(time);
+
+// for (y = 2; y <time.length; y++) {
+//     var years = time.item(y).innerHTML;
+//     console.log(years);
+//     yearList.push(years);
+// }
+
+var yearList = [];
 
 for (l = 2; l < correctYear.length; l++) {
     var awesomeYears = correctYear[l].innerHTML;
-    console.log(awesomeYears);
+    yearList.push(awesomeYears);
 }
+
+// DATA 
+//BELGIQUE
+
+var Belgique = tableOne.rows.item(2).cells;
+var BelgiqueNom = Belgique.item(1).innerHTML;
+var BelgiqueNombres = [];
+
+for (b = 2; b <Belgique.length; b++) {
+    var BelgiqueNrs = Belgique.item(b).innerHTML;
+    BelgiqueNombres.push(BelgiqueNrs);
+}
+
+// console.log(Belgique);
+// console.log(BelgiqueNom);
+// console.log(BelgiqueNrs);
+// console.log(BelgiqueNombres);
+
+var dataBelgique = {
+    label: BelgiqueNom,
+    data: BelgiqueNombres,
+};
+
+//BULGARIE
+
+var Bulgarie = tableOne.rows.item(3).cells;
+var BulgarieNom = Bulgarie.item(1).innerHTML;
+var BulgarieNombres = [];
+
+for (bb = 2; bb <Bulgarie.length; bb++) {
+    var BulgarieNrs = Bulgarie.item(bb).innerHTML;
+    BulgarieNombres.push(BelgiqueNrs);
+}
+
+// console.log(Bulgarie);
+// console.log(BulgarieNom);
+// console.log(BulgarieNrs);
+// console.log(BulgarieNombres);
+
+var dataBulgarie = {
+    label: BulgarieNom,
+    data: BulgarieNombres,
+};
+
+//REP TCHEQUE
+
+var RepTcheque = tableOne.rows.item(4).cells;
+var RepTchequeNom = RepTcheque.item(1).innerHTML;
+var RepTchequeNombres = [];
+
+for (rt = 2; rt <RepTcheque.length; rt++) {
+    var RepTchequeNrs = RepTcheque.item(rt).innerHTML;
+    RepTchequeNombres.push(RepTchequeNrs);
+}
+
+// console.log(RepTcheque);
+// console.log(RepTchequeNom);
+// console.log(RepTchequeNrs);
+// console.log(RepTchequeNombres);
+
+var dataReptcheque = {
+    label: RepTchequeNom,
+    data: RepTchequeNombres,
+};
+
+//DANMARK
+
+var Danmark = tableOne.rows.item(5).cells;
+var DanmarkNom = Danmark.item(1).innerHTML;
+var DanmarkNombres = [];
+
+for (d = 2; d <Danmark.length; d++) {
+    var DanmarkNrs = Danmark.item(d).innerHTML;
+    DanmarkNombres.push(DanmarkNrs);
+}
+
+// console.log(Danmark);
+// console.log(DanmarkNom);
+// console.log(DanmarkNrs);
+// console.log(DanmarkNombres);
+
+var dataDanmark = {
+    label: DanmarkNom,
+    data: DanmarkNombres,
+};
+
+//ALLEMAGNE
+
+var Allemagne = tableOne.rows.item(6).cells;
+var AllemagneNom = Allemagne.item(1).innerHTML;
+var AllemagneNombres = [];
+
+for (al = 2; al <Allemagne.length; al++) {
+    var AllemagneNrs = Allemagne.item(al).innerHTML;
+    AllemagneNombres.push(AllemagneNrs);
+}
+
+// console.log(Allemagne);
+// console.log(AllemagneNom);
+// console.log(AllemagneNrs);
+// console.log(AllemagneNombres);
+
+var dataAllemagne = {
+    label: AllemagneNom,
+    data: AllemagneNombres,
+};
+
+//ESTONIE
+
+var Estonie = tableOne.rows.item(7).cells;
+var EstonieNom = Estonie.item(1).innerHTML;
+var EstonieNombres = [];
+
+for (es = 2; es <Estonie.length; es++) {
+    var EstonieNrs = Estonie.item(es).innerHTML;
+    EstonieNombres.push(EstonieNrs);
+}
+
+// console.log(Estonie);
+// console.log(EstonieNom);
+// console.log(EstonieNrs);
+// console.log(EstonieNombres);
+
+var dataEstonie = {
+    label: EstonieNom,
+    data: EstonieNombres,
+};
+
+
+
+
+var crimiData = {
+    labels: yearList,
+    datasets: [dataBelgique, dataBulgarie, dataReptcheque, dataDanmark, dataAllemagne, dataEstonie]
+};
 
 // CHART.JS CODE
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-
-        
-        labels: [awesomeYears],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    type: 'line',
+    data: crimiData,
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+            scaleOverride: true,
+            scaleStartValue: 0,
+            scaleSteps: 20,
+            scaleStepWidth: 300,
     }
 });
