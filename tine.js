1ad55b143a5808efb376cde37327a8b830c13e37
“[tine <[tinevancorenland@gmail.com]>
2019-07-26 12:16:40 +0200

    data Belgium in graph

--- tine.js
+++ tine.js
@@ -11,35 +11,76 @@ header.appendChild(canvas);
 var table = document.getElementById("table1");
 console.log(table);
 var rowYears = table.rows[1];
-console.log(rowYears);
-var rowCells = table.rows[1].cells;
-console.log(rowCells);
+console.log(rowYears.cells);
 
 var yearList = [];
 
-for (y = 2; y < rowCells.length; y++) {
-  var years = rowCells[y].firstChild.data;
-  console.log(years);
+for (y = 2; y < rowYears.cells.length; y++) {
+  var years = rowYears.cells[y].firstChild.data;
   yearList.push(years);
-  console.log(yearList);
 }
+
+// Country name for dataset 1
+
+var rowBe = table.rows[2];
+console.log(rowBe);
+
+var BeName = rowBe.cells[1].firstChild.data;
+console.log(BeName);
+
+var BeData = [];
+
+for (Be = 2; Be < rowBe.cells.length; Be++) {
+  var BeNrs = parseInt(rowBe.cells[Be].firstChild.data);
+  BeData.push(BeNrs);
+}
+
+console.log(BeNrs);
+console.log(yearList);
+console.log(BeData);
+
+// Options
+
+// var options = {
+//   scales: {
+//     yAxes: [
+//       {
+//         ticks: {
+//           min: -2000,
+//           max: 8000
+//         }
+//       }
+//     ]
+//   }
+// };
 
 // Chart JS basic function
 
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById("myChart").getContext("2d");
 var chart = new Chart(ctx, {
-  type: 'line',
+  type: "line",
 
   data: {
     labels: yearList,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
-    }]
+    datasets: [
+      {
+        label: BeName,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: BeData
+      }
+    ]
   },
 
-
-  options: {}
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
 });
