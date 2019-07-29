b78e5a3514292029ee441187646ef59337152732
“[tine <[tinevancorenland@gmail.com]>
2019-07-29 10:56:11 +0200

    add a loop to go through all rows, but results in ALL data and not data per country, can make it work by putting each country in graph seperatly

--- tine.js
+++ tine.js
@@ -33,18 +33,41 @@ for (Be = 2; Be < rowBe.cells.length; Be++) {
   BeData.push(BeNrs);
 }
 
+// Country dataset 2
+
+var rowBu = table.rows[3];
+console.log(rowBu);
+
+var BuName = table.rows[3].cells[1].firstChild.data;
+
+var BuData = [];
+
+for (d = 2; d < table.rows[3].cells.length; d++) {
+  var BuNrs = parseInt(table.rows[3].cells[d].firstChild.data);
+  BuData.push(BuNrs);
+}
+
 // Loop to pull data from every country
 
-console.log(table.rows);
-
 var allRows = [];
+var allCountries = [];
+var allData = [];
 
 for (ro = 2; ro < table.rows.length; ro++) {
   var currentRow = table.rows[ro].cells;
   allRows.push(currentRow);
+  var currentCountry = table.rows[ro].cells[1].firstChild.data;
+  allCountries.push(currentCountry);
+
+  for (nrs = 2; nrs < table.rows[ro].cells.length; nrs++) {
+    var currentData = parseInt(table.rows[ro].cells[nrs].firstChild.data);
+    allData.push(currentData);
+  }
 }
 
 console.log(allRows);
+console.log(allCountries);
+console.log(allData);
 
 // Chart js basic data
 
@@ -57,9 +80,17 @@ var chart = new Chart(ctx, {
     datasets: [
       {
         label: BeName,
-        // backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: BeData
+        data: BeData,
+        lineTension: 0,
+        fill: false,
+        borderColor: "pink"
+      },
+      {
+        label: BuName,
+        data: BuData,
+        lineTension: 0,
+        fill: false,
+        borderColor: "purple"
       }
     ]
   },
